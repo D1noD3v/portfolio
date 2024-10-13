5932d3f668048f4fbbb582a6c3b1dcccc9c185a9
Dino Norén <brankovicdino851@gmail.com>
2024-10-13 14:46:48 +0200

    changed from HTML anchor to Next Link

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import React from 'react';
 import logo from '/home/dinodev/portfolio/public/terminal.png';
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 function Header() {
@@ -10,10 +11,7 @@ function Header() {
         <Image src={logo} alt="Terminal logo" className="logo" />
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/services">Services</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <Link href={"/"}>Home</Link>
             </ul>
         </nav>
     </header>
