e85367a0811eac207cfc31ff83ed8f306022a72a
Dino Norén <brankovicdino851@gmail.com>
2024-10-22 16:50:21 +0200

    removed previous changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config) {
-    config.module.rules.push({
-      test: /\.(ttf|otf|eot|woff|woff2)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          outputPath: '@public/fonts/', // or wherever you'd like to store the font files
-          publicPath: '/_next/static/fonts/', // or adapt as per your project
-        },
-       }
-      }
-    )
-    return config;
-  }
-}
+const nextConfig = {}
 
 export default nextConfig;
