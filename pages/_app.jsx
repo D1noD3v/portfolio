df5e97e15ac5ddcf50a30212fe420f8b59b34072
Dino Norén <brankovicdino851@gmail.com>
2024-10-22 15:31:50 +0200

    todo: fix font + updated next.js

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/public/style.css";
 import "@/public/terminal.png";
+import "@/public/fonts/KronaOne-Regular.ttf"
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
