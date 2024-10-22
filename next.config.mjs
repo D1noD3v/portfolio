b082c51cc2349a192a1b5c12bfec0bd21d29012f
Dino Norén <brankovicdino851@gmail.com>
2024-10-22 16:48:44 +0200

    todo: font is still fucked idk wtf

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config) {
+    config.module.rules.push({
+      test: /\.(ttf|otf|eot|woff|woff2)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '@public/fonts/', // or wherever you'd like to store the font files
+          publicPath: '/_next/static/fonts/', // or adapt as per your project
+        },
+       }
+      }
+    )
+    return config;
+  }
+}
 
 export default nextConfig;
