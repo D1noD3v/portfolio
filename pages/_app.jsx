99da3501da4bbcb6e48843ddcedc0a3ddf7707c8
Dino Norén <brankovicdino851@gmail.com>
2024-10-23 16:51:14 +0200

    removed font files

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
 import "@/public/style.css";
 import "@/public/terminal.png";
-import "@/public/fonts/KronaOne-Regular.ttf"
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
